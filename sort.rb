6cd2e120c58d38486234bee21c62569ab16c83fa
SUZUKI Kazuaki <campanalbero@gmail.com>
2014-10-16 04:52:11 +0900

    create entity class

--- sort.rb
+++ sort.rb
@@ -6,20 +6,14 @@ require "sequel"
 
 $log = Logger.new("batch.log")
 
-module Db
-	def insert_into_db(relative_path)
-		begin
-			exif = Exif.new(relative_path)
-			digest = Digest::MD5.file(relative_path).to_s
-			date = exif.get_date.to_s
-			model = exif.get_model
+Process.setrlimit(Process::RLIMIT_NOFILE, 4096, 65536)
 
-			dir = "photo/"
-			db = Sequel.sqlite(dir + "photo.db")
-			relative_path_from_dir = Pathname.new(relative_path).relative_path_from(Pathname.new(dir))
-			db[:photo].insert(:path => relative_path_from_dir.to_s, :md5 => digest, :date_time_original => date, :model => model)
+module Db
+	def insert_into_db(entity)
+		begin
+			db[:photo].insert(:path => entity.path, :md5 => entity.md5, :date_time_original => entity.date, :model => entity.model)
 		rescue => e
-	 		$log.error(__method__.to_s + ", " + File.expand_path(relative_path) + ", " + e.message)
+	 		$log.error(__method__.to_s + ", " + File.expand_path(entity.path) + ", " + e.message)
 		end
 	end
 
@@ -39,12 +33,58 @@ module Db
 	module_function :insert_into_queue
 end
 
-class Exif
-
-	def initialize(path)
-		@exif = MiniExiftool::new(path)
+class Entity
+	# 引数のタイムスタンプから yyyy/mm/dd という文字列を返す
+	def yyyymmdd(timestump)	
+		yyyy = timestump.strftime("%Y")
+		mm = timestump.strftime("%m")
+		dd = timestump.strftime("%d")
+		yyyy + "/" + mm + "/" + dd
 	end
 
+	# 作成日を得る
+	def get_date(exif)
+		if exif.MediaModifyDate != nil
+			exif.MediaModifyDate
+		elsif exif.DateTimeOriginal != nil
+			exif.DateTimeOriginal
+		else
+			exif.FileModifyDate
+		end
+	end
+
+	# 撮影したカメラモデルを得る（あくまで俺の持ってたカメラ）
+	def get_model(exif)
+		if exif["Model"] != nil
+			exif["Model"]
+		elsif exif["Model-jpn"] != nil
+			exif["Model-jpn"]
+		elsif exif.filename.include?("MAH0")
+			"DSC-HX5V"
+		elsif exif.filename.include?("video-")
+			"GT-S5660"
+		elsif exif.filename.include?("VID_") || exif.filename.include?("PANO_")
+			"Nexus 5"
+		else
+			"????"
+		end
+	end
+
+    def initialize(current_path)
+         exif = MiniExiftool.new(current_path)
+         @md5 = Digest::MD5.file(current_path).to_s
+         @date = get_date(exif)
+         @model = get_model(exif)
+         @path = yyyymmdd(@date) + "/" + File.basename(current_path)
+     end
+
+     attr_reader :path
+     attr_reader :md5
+     attr_reader :date
+     attr_reader :model
+end
+
+class Exif
 	def self.image?(path)
 		extension = File.extname(path).downcase
 		targets = [".jpg", ".nef", ".cr2"]
@@ -60,34 +100,6 @@ class Exif
 	def self.target?(path)
 		image?(path) or movie?(path)
 	end
-
-	# 作成日を得る
-	def get_date
-		if @exif.MediaModifyDate != nil
-			@exif.MediaModifyDate
-		elsif @exif.DateTimeOriginal != nil
-			@exif.DateTimeOriginal
-		else
-			@exif.FileModifyDate
-		end
-	end
-
-	# 撮影したカメラモデルを得る（あくまで俺の持ってたカメラ）
-	def get_model
-		if @exif["Model"] != nil
-			@exif["Model"]
-		elsif @exif["Model-jpn"] != nil
-			@exif["Model-jpn"]
-		elsif @exif.filename.include?("MAH0")
-			"DSC-HX5V"
-		elsif @exif.filename.include?("video-")
-			"GT-S5660"
-		elsif @exif.filename.include?("VID_") || @exif.filename.include?("PANO_")
-			"Nexus 5"
-		else
-			"????"
-		end
-	end
 end
 
 # 引数(path)の末尾が / になったものを返す
@@ -99,14 +111,6 @@ def directorize(path)
 	end
 end
 
-# 引数のタイムスタンプから yyyy/mm/dd という文字列を返す
-def yyyymmdd(timestump)	
-	yyyy = timestump.strftime("%Y")
-	mm = timestump.strftime("%m")
-	dd = timestump.strftime("%d")
-	yyyy + "/" + mm + "/" + dd
-end
-
 puts("from, to")
 # photo/ フォルダ以下に決め打ち
 # tmp/ フォルダ以下に決め打ち
@@ -114,15 +118,14 @@ Dir.glob(["tmp/**/*"]) do |f|
 	if not Exif.target?(f) then
 		next
 	end
-	exif = Exif.new(f)
-	timestump = exif.get_date
-	new_dir = "photo/" + yyyymmdd(timestump)
-	dst = new_dir + "/" + File.basename(f)
+	entity = Entity.new(f)
+	dst = "photo/" + entity.path
+	new_dir = File::dirname(dst)
 	FileUtils::mkdir_p(new_dir) unless FileTest.exist?(new_dir)
 	if (not FileTest.exist?(dst))
 		puts(File.expand_path(f) + ", " + File.expand_path(dst))
 		File.rename(f, dst)
-		Db.insert_into_db(dst)
+		Db.insert_into_db(entity)
 		Db.insert_into_queue(dst)
 	else
 		$log.error(f + " already exist")
