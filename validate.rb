6a0f06c6b067121b1ffd5834f0982688aecbab89
SUZUKI Kazuaki <campanalbero@gmail.com>
2015-01-29 10:54:12 +0900

    add lost-file validation

--- validate.rb
+++ validate.rb
@@ -6,20 +6,30 @@ require "digest/md5"
 DB = Sequel.sqlite('photo/photo.db')
 photo = DB[:photo].all
 broken_files = []
-
+lost_files = []
 # use "order by" if you resume
 DB.fetch("select path, md5 from photo"){|item|
-	current_md5 = Digest::MD5.file("photo/" + item[:path])
+	path = "photo/" + item[:path]
+
+	if (not File::exist?(path))
+		lost_files.push(path)
+		next
+	end
+
+	current_md5 = Digest::MD5.file(path)
 	past_md5 = item[:md5]
 	if (current_md5 != past_md5)
-		broken_files.push(item[:path])
+		broken_files.push(path)
 	end
 }
 
-if not broken_files.empty?
-	puts "broken files are ..."
-	puts broken_files.sort!
+if broken_files.empty? and lost_files.empty?
+	puts "All registered files are OK."
 else
-	puts "ALL REGISTERED FILES ARE NOT BROKEN."
+	puts "broken files are..."
+	puts broken_files
+	puts "----"
+	puts "lost_files are ..."
+	puts lost_files
 end
 
